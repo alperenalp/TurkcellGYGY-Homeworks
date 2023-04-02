2e607ae4691ae745d721c2277d6b924935a45f17
alperenalp <alperen.alp045@gmail.com>
2023-04-02 22:35:16 +0300

    Task 6 Liskov Substituon Principle complete with reservation example

---
+++ Task6LiskovSubstituonPrensible/Task6LiskovSubstituonPrensible/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task6LiskovSubstituonPrensible;
+
+Console.WriteLine("Hello, World!");
+
+
+var reserveHotelRoom = ReservingService.ReserveFactory(DateTime.Now, 30);
+reserveHotelRoom.CreateReserve();
+
+
+var reserveVehicle = ReservingService.ReserveFactory(DateTime.Now, 30, "VOLVO");
+reserveVehicle.CreateReserve();
+
+
+var reserveConferencePlace = ReservingService.ReserveFactory(DateTime.Now, 30, 500);
+reserveConferencePlace.CreateReserve();
---
+++ Task6LiskovSubstituonPrensible/Task6LiskovSubstituonPrensible/ReservingService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6LiskovSubstituonPrensible
+{
+    public static class ReservingService
+    {
+        public static IReservable ReserveFactory(DateTime date, int days)
+        {
+            return new Hotel() { ReserveDate = date, ReserveDaysCount = days };
+        }
+
+        public static IReservable ReserveFactory(DateTime date, int days, string brand)
+        {
+            return new Vehicle() { ReserveDate = date, ReserveDaysCount = days, Brand = brand };
+        }
+
+        public static IReservable ReserveFactory(DateTime date, int days, int capacity)
+        {
+            return new ConferencePlace() { ReserveDate = date, ReserveDaysCount = days, Capacity = capacity };
+        }
+
+    }
+
+    public interface IReservable
+    {
+        public void CreateReserve();
+    }
+
+    public class Hotel : IReservable
+    {
+        public DateTime ReserveDate { get; set; }
+        public int ReserveDaysCount { get; set; }
+
+        public void CreateReserve()
+        {
+            Console.WriteLine($"{ReserveDate} tarihinde {ReserveDaysCount} günlük Otel odası rezervasyonu yapıldı.");
+        }
+    }
+
+    public class Vehicle : IReservable
+    {
+        public DateTime ReserveDate { get; set; }
+        public int ReserveDaysCount { get; set; }
+        public string Brand { get; set; }
+
+        public void CreateReserve()
+        {
+            Console.WriteLine($"{ReserveDate} tarihinde {ReserveDaysCount} günlük {Brand} marka araç rezervasyonu yapıldı.");
+        }
+    }
+
+    public class ConferencePlace : IReservable
+    {
+        public DateTime ReserveDate { get; set; }
+        public int ReserveDaysCount { get; set; }
+        public int Capacity { get; set; }
+
+        public void CreateReserve()
+        {
+            Console.WriteLine($"{ReserveDate} tarihinde {ReserveDaysCount} günlük {Capacity} kapasiteli konferans salonu rezervasyonu yapıldı.");
+        }
+    }
+}
