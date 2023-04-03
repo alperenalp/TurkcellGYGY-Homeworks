75343e8196b87d501299e022738689b448cca1da
alperenalp <alperen.alp045@gmail.com>
2023-04-03 18:32:49 +0300

    principle applied

---
+++ Task7InterfaceSegregationPrinciple/Task7InterfaceSegregationPrinciple/IdealCode.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7InterfaceSegregationPrincipleIdealCode
+{
+    interface Savable
+    {
+        void Save();
+    }
+    interface Editable
+    {
+        void Edit();
+    }
+    interface Printable
+    {
+        void Print();
+    }
+
+    class Word : Savable, Editable, Printable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Yazdırma işlemleri...");
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Kaydetme işlemleri...");
+        }
+
+        public void Edit()
+        {
+            Console.WriteLine("Düzenleme işlemleri...");
+        }
+    }
+
+    class PowerPoint : Savable,Editable
+    {
+        public void Save()
+        {
+            Console.WriteLine("Kaydetme işlemleri...");
+        }
+
+        public void Edit()
+        {
+            Console.WriteLine("Düzenleme işlemleri...");
+        }
+    }
+
+    class AdobeAcrobat : Printable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Yazdırma işlemleri...");
+        }
+    }
+}
---
+++ Task7InterfaceSegregationPrinciple/Task7InterfaceSegregationPrinciple/NotIdealCode.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7InterfaceSegregationPrinciple
+{
+    interface IDocumenter
+    {
+        void Save();
+        void Edit();
+        void Print();
+    }
+
+    class Word : IDocumenter
+    {
+        public void Print()
+        {
+            Console.WriteLine("Yazdırma işlemleri...");
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Kaydetme işlemleri...");
+        }
+
+        public void Edit()
+        {
+            Console.WriteLine("Düzenleme işlemleri...");
+        }
+    }
+
+    class PowerPoint : IDocumenter
+    {
+        public void Print() => throw new NotSupportedException();
+
+        public void Save()
+        {
+            Console.WriteLine("Kaydetme işlemleri...");
+        }
+
+        public void Edit()
+        {
+            Console.WriteLine("Düzenleme işlemleri...");
+        }
+    }
+
+    class AdobeAcrobat : IDocumenter
+    {
+        public void Print()
+        {
+            Console.WriteLine("Yazdırma işlemleri...");
+        }
+
+        public void Save() => throw new NotSupportedException();
+
+        public void Edit() => throw new NotSupportedException();
+    }
+}
