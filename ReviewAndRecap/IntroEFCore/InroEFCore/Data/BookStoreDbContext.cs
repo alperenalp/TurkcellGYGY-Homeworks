43eaf86de76bed321c2f2073959bb2a371f32b75
alperenalp <alperen.alp045@gmail.com>
2023-05-14 01:42:26 +0300

    Created sample book store database via Migration

---
+++ ReviewAndRecap/IntroEFCore/InroEFCore/Data/BookStoreDbContext.cs
@@ -0,0 +1,34 @@
+﻿using InroEFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InroEFCore.Data
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=bookStoreSampleDb;Integrated Security=True;TrustServerCertificate=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().Property(b => b.Title).IsRequired()
+                                                              .HasMaxLength(250);
+
+            modelBuilder.Entity<Review>().HasOne(r => r.Book)
+                                         .WithMany(b => b.Reviews)
+                                         .HasForeignKey(r => r.BookId)
+                                         .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
--- ReviewAndRecap/IntroEFCore/InroEFCore/Program.cs
+++ ReviewAndRecap/IntroEFCore/InroEFCore/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using InroEFCore.Data;
+using InroEFCore.Models;
+
 Console.WriteLine("Hello, World!");
+
+var dbContext = new BookStoreDbContext();
+
+Author author = new() { Name = "Jules", LastName = "Verne" };
+dbContext.Authors.Add(author);
+dbContext.Books.Add(new() { Title = "Denizler Altında 20.000 Fersah", Description = "Açıklama", Authors = new List<Author> { author } });
+
+dbContext.SaveChanges();
