ba1d30c6af7cc3a2cb3c753312371842d600ae9b
alperenalp <alperen.alp045@gmail.com>
2023-05-16 02:42:03 +0300

    Update IMovieService

--- ReviewAndRecap/Movies/Movies.Application/Services/IMovieService.cs
+++ ReviewAndRecap/Movies/Movies.Application/Services/IMovieService.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Movies.Application.DTOs.Requests;
+using Movies.Application.DTOs.Responses;
 
 namespace Movies.Application.Services
 {
-    internal interface IMovieService
+    public interface IMovieService
     {
+        Task<int> CreateNewMovie(CreateNewMovieRequest createNewMovie);
+        Task UpdateMovie(UpdateMovieRequest updateMovie);
+        Task<IEnumerable<MovieListResponse>> GetAllMovies();
+
+        Task AddPlayerToMovie(int movieId, List<int> players);
     }
 }
