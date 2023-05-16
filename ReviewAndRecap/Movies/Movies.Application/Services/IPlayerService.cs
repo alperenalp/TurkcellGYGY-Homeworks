b7a9307bbb511dad0b9e59ba570c4d0ed0d65739
alperenalp <alperen.alp045@gmail.com>
2023-05-16 03:02:41 +0300

    Completed PlayerService

---
+++ ReviewAndRecap/Movies/Movies.Application/Services/IPlayerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Application.Services
+{
+    public interface IPlayerService
+    {
+        Task<int> CreateNewPlayer(CreateNewPlayerRequest request);
+        Task<IEnumerable<PlayerListResponse>> GetPlayerLists();
+        Task UpdatePlayer(UpdatePlayerRequest request);
+    }
+}
